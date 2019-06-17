286050129106e449182d3cc0e78f0aaccd360727
foxbandit <feenstra32@gmail.com>
2019-06-17 07:18:55 -0700

    Adding MultiEvent Triggers

---
+++ LevelScripts/MultipleEventTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MultipleEventTrigger : MonoBehaviour
+{
+    public int numberOfTriggers = 0;
+    [Sirenix.OdinInspector.ReadOnly] public int triggerCount = 0;
+    public UnityEvent keyAction;
+
+    public void IncrementTrigger()
+    {
+        triggerCount += 1;
+        CheckTriggerConditions();
+    }
+
+    public void DecrementTrigger()
+    {
+        triggerCount -= 1;
+        CheckTriggerConditions();
+    }
+
+    private void CheckTriggerConditions()
+    {
+        if (triggerCount == numberOfTriggers)
+            keyAction.Invoke();
+    }
+}
